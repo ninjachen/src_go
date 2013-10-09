88b5261f3bacefcfbc167e58bd31c2ed9c9cc73b
ninja <jiachenning@gmail.com>
2013-10-09 23:31:35 +0800

    import package test

--- main.go
+++ main.go
@@ -2,40 +2,15 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"mymath"
+	"mysort"
 )
 
-var store = NewURLStore()
-
 func main() {
-	http.HandleFunc("/", Redirect)
-	http.HandleFunc("/add", Add)
-	http.ListenAndServe(":8080", nil)
-}
+	fmt.Printf("Hello, world.  Sqrt(2) = %v\n", mymath.Sqrt(2))
+	fmt.Printf("Hello, world.  bubblesort = %s\n", mysort.bubbleSort())
 
-func Redirect(w http.ResponseWriter, r *http.Request) {
-	key := r.URL.Path[1:]
-	url := store.Get(key)
-	if url == "" {
-		http.NotFound(w, r)
-		return
-	}
-	http.Redirect(w, r, url, http.StatusFound)
+	// http.HandleFunc("/", Redirect)
+	// http.HandleFunc("/add", Add)
+	// http.ListenAndServe(":8080", nil)
 }
-
-func Add(w http.ResponseWriter, r *http.Request) {
-	url := r.FormValue("url")
-	if url == "" {
-		fmt.Fprint(w, AddForm)
-		return
-	}
-	key := store.Put(url)
-	fmt.Fprintf(w, "http://localhost:8080/%s", key)
-}
-
-const AddForm = `
-<form method="POST" action="/add">
-URL: <input type="text" name="url">
-<input type="submit" value="Add">
-</form>
-`
---
+++ src/mysort/mysort.go
@@ -0,0 +1,12 @@
+package mysort
+
+import ()
+
+func bubbleSort() string {
+	return "Hello,World Ninja"
+	//result := []int{0, 9, 9}
+
+	//遍历原数组
+	//比较当前数据和后一个数据，如果当前数据大则交换位置
+	//遍历到n-1个结束
+}
