e5f4ebf64e8a657f99ce1234cf4c09b53e371911
ninja <jiachenning@gmail.com>
2013-10-10 20:59:30 +0800

    new file

---
+++ src/mysort/XXXXXsort.go
@@ -0,0 +1,14 @@
+package mysort
+
+import (
+	"fmt"
+)
+
+func BubbleSort(x int) int {
+	fmt.Print("%d%d", "Hello,World Ninja", x)
+	//result := []int{0, 9, 9}
+	return x
+	//遍历原数组
+	//比较当前数据和后一个数据，如果当前数据大则交换位置
+	//遍历到n-1个结束
+}
