98abf3866ad97c9956bad3420d6be85ab67f2c30
ninja <jiachenning@gmail.com>
2013-10-12 00:13:47 +0800

    bubble

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"mymath"
 	"mysort"
 )
 
 func main() {
-	fmt.Printf("Hello, world.  Sqrt(2) = %v\n", mymath.Sqrt(2))
-	fmt.Printf("Hello, world.  bubblesort = %d\n", mysort.BubbleSort(1))
+	// fmt.Printf("Hello, world.  Sqrt(2) = %v\n", mymath.Sqrt(2))
+	var slice []int
+	slice = []int{1, 3, 3, 11, 5, 6}
+	slice = mysort.BubbleSort(slice)
+	// fmt.Printf("Hello, world.  bubblesort = %v\n", slice)
 
 	// http.HandleFunc("/", Redirect)
 	// http.HandleFunc("/add", Add)
